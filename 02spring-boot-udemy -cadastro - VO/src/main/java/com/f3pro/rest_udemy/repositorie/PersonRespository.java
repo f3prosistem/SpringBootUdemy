f57ea80d0f54f66617575977d556b68d5b2e563a
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-04-21 21:49:54 -0300

    integração da patter VO no projeto

--- 02spring-boot-udemy -cadastro - VO/src/main/java/com/f3pro/rest_udemy/repositorie/PersonRespository.java
+++
@@ -1,9 +0,0 @@
-package com.f3pro.rest_udemy.repositorie;
-
-import com.f3pro.rest_udemy.modal.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRespository extends JpaRepository<Person, Long> {
-}
